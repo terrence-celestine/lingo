59d8c2a6691001ddf7fa000d069ec8bd332907d1
Terrence <terrence.butler89@gmail.com>
2026-06-19 08:23:18 -0700

    feat: added progress & leaderboard screens

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import QuizScreen from "./screens/QuizScreen";
 import CompleteScreen from "./screens/CompleteScreen";
+import LeaderboardScreen from "./screens/LeaderboardScreen";
+import ProgressScreen from "./screens/ProgressScreen";
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
         <Route path="/" element={<HomeScreen />} />
         <Route path="/quiz/:lessonId" element={<QuizScreen />} />
         <Route path="/complete" element={<CompleteScreen />} />
+        <Route path="/leaderboard" element={<LeaderboardScreen />} />
+        <Route path="/progress" element={<ProgressScreen />} />
       </Routes>
     </BrowserRouter>
   );
